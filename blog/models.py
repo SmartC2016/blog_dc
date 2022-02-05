188f286f735fb19f366b5aec9864248a149a9738
Christian Hetmann <christian.hetmann@gmail.com>
2022-02-05 18:07:58 +0100

    Started with Comments

--- blog/models.py
+++ blog/models.py
@@ -11,9 +11,7 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now=True)
@@ -24,3 +22,18 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self) -> str:
+        return f"Comment {self.body[:50]} by {self.name}"
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)  # only published Posts, no drafts
     template_name = "index.html"
+    context_object_name = "posts"
 
 
 class PostDetail(generic.DetailView):
